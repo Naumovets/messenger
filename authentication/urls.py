05286e94dd8abea4eb0ed645ad09970a775b2bc7
Naumovets <naumovets.daniil2003@mail.ru>
2023-05-19 05:39:25 +0300

    Transfer to REST API apps

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-
-urlpatterns = [
-    path('logout/', user_logout, name='logout'),
-    path('login/', LoginUser.as_view(), name='login'),
-    path('register/', RegisterUser.as_view(), name='register')
-]
--- friend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from friend.views import ListFriends
-
-urlpatterns = [
-    path('', ListFriends.as_view(), name='friends'),
-
-]
